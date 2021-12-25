dc5305e657d61543c35141f8f2b04751dc4042e5
Lakshmipathi rao Devalla <lakshmipathirao.devalla@stud.uni-hannover.de>
2021-12-25 22:53:01 +0100

    Trying diff model

--- src/model.py
+++ src/model.py
@@ -2,8 +2,64 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+# class SiameseNetwork(nn.Module):
+#     def __init__(self):
+#         super(SiameseNetwork, self).__init__()
+        
+#         # Setting up the Sequential of CNN Layers
+#         self.cnn1 = nn.Sequential(
+            
+#             nn.Conv2d(1, 96, kernel_size=11,stride=1),
+#             nn.ReLU(inplace=True),
+#             nn.LocalResponseNorm(5,alpha=0.0001,beta=0.75,k=2),
+#             nn.MaxPool2d(3, stride=2),
+            
+#             nn.Conv2d(96, 256, kernel_size=5,stride=1,padding=2),
+#             nn.ReLU(inplace=True),
+#             nn.LocalResponseNorm(5,alpha=0.0001,beta=0.75,k=2),
+#             nn.MaxPool2d(3, stride=2),
+#             nn.Dropout2d(p=0.3),
+
+#             nn.Conv2d(256,384 , kernel_size=3,stride=1,padding=1),
+#             nn.ReLU(inplace=True),
+#             nn.Conv2d(384,256 , kernel_size=3,stride=1,padding=1),
+#             nn.ReLU(inplace=True),
+#             nn.MaxPool2d(3, stride=2),
+#             nn.Dropout2d(p=0.3),
+
+#         )
+        
+#         # Defining the fully connected layers
+#         self.fc1 = nn.Sequential(
+#             nn.Linear(30976, 1024),
+#             nn.ReLU(inplace=True),
+#             nn.Dropout2d(p=0.5),
+            
+#             nn.Linear(1024, 128),
+#             nn.ReLU(inplace=True),
+            
+#             nn.Linear(128,2))
+        
+#     def forward_once(self, x):
+#         # Forward pass 
+#         output = self.cnn1(x)
+#         output = output.view(output.size()[0], -1)
+#         output = self.fc1(output)
+#         return output
+
+#     def forward(self, input1, input2):
+#         # forward pass of input 1
+#         output1 = self.forward_once(input1)
+#         # forward pass of input 2
+#         output2 = self.forward_once(input2)
+#         return output1, output2
+
+
+
+
+
+
 """ From: https://github.com/kevinzakka/one-shot-siamese/blob/master/model.py """
-""" Currently ignoring the 2nd fully copnected layer, which is the trained similarity metric """
 
 class SiameseNetwork(nn.Module):
     def __init__(self, lastLayer = True, pretrained = False):
