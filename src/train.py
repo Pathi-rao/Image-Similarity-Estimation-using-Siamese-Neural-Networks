7a51dfc68006a94122f1c5243de14409f65edaf4
Lakshmipathi rao Devalla <lakshmipathirao.devalla@stud.uni-hannover.de>
2021-12-25 22:53:40 +0100

    added model validation

--- src/train.py
+++ src/train.py
@@ -1,35 +1,22 @@
 import torch
-import torchvision.datasets as dset
-import torchvision.transforms as transforms
-from torch.utils.data import DataLoader
 from torch import optim
-import torchvision
-import torch.nn.functional as F
 import time
-
 import matplotlib.pyplot as plt
-import argparse
 
 from loss import ContrastiveLoss
 import datahandler as dl
-from misc import Utils
 from model import SiameseNetwork
-
-# parser = argparse.ArgumentParser(description="Siamese CNN")
-# parser.add_argument('--epochs', metavar = 'e', type = int, required = True)
-# parser.add_argument('--lr', metavar = 'l', type = float, required = True)
-# parser.add_argument('--dropout', metavar = 'd', type = float, required = True)
-# parser.add_argument('--saved_data', metavar = 's', type = int, default= 1, required = False)
-# args = vars(parser.parse_args())
 
 root_dir = '..\..\Dataset\MVTEC_AD'
 epochs = 50
 lear_rate = 0.0005
 trainbatchsize = 4
 validbatchsize = 4
+testbatchsize = 1
 
-train_loader, valid_loader = dl.pre_processor(root_dir=root_dir, trainbatchsize=trainbatchsize, 
-                                                validbatchsize=validbatchsize)
+train_loader, valid_loader, test_loader = dl.pre_processor(root_dir=root_dir, trainbatchsize=trainbatchsize, 
+                                                validbatchsize=validbatchsize,
+                                                testbatchsize=testbatchsize)
 
 net = SiameseNetwork().cuda()
 # net.train()
@@ -37,15 +24,15 @@ criterion = ContrastiveLoss().cuda()
 optimizer = optim.Adam(net.parameters(), lr = lear_rate )
 scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 1, gamma=0.95)
 
-train_losses = []
-val_losses = []
+train_loss_history = [] 
+val_loss_history = [] 
 
 best_val = 0.1
 
 start_time = time.time()
 
-for epoch in range( epochs):
-
+for epoch in range(epochs):
+    # print(epoch)
     net.train()
     running_loss = 0
 
@@ -65,35 +52,27 @@ for epoch in range( epochs):
 
         running_loss += loss_contrastive.item()
 
-    # print('My loss... ', running_loss /img0.shape[0] )
-    # print('cjv loss....', running_loss/i)
-    train_losses.append(running_loss /len(train_loader))
-
-    if epoch % 2 == 0:
-        val_loss = 0
+    if epoch % 2 == 0: # validate for every 2 epochs
+        train_loss_history.append(running_loss/len(train_loader))
+        # print(epoch)
+        print("Epoch number {}\t Train loss {}\t".format(epoch, running_loss/len(train_loader)))
         net.eval()
+        val_loss = 0
         with torch.no_grad():
             for i , data in enumerate(valid_loader, 0):
                 img_0, img_1, label_ = data
                 img_0, img_1 , label_ = img_0.cuda(), img_1.cuda() , label_.cuda()
                 output1, output2 = net(img_0, img_1)
-                # distance = torch.sigmoid(F.pairwise_distance(output1, output2))
                 loss_val = criterion(output1, output2, label_)
-                val_loss += loss_val.item() 
+                val_loss += loss_val.item()
+                val_loss_history.append(val_loss/len(valid_loader))
 
-        # print('validation image shape is:   ', img_0.shape[0])
-        val_losses.append(val_loss/len(valid_loader))
-
-        print('Epoch : ',epoch, "\t Train loss: {:.2f}".format(running_loss/len(train_loader)),
-            "\t Validation loss: {:.2f}".format(val_loss/len(valid_loader)))
-
-        if val_loss/len(valid_loader) < best_val:
-            best_val = val_loss/len(valid_loader)
-            PATH = 'models/best_model_val_loss.pth'
-            torch.save(net, PATH)
-            print()
-            print("Saved best model at epoch:  ", epoch)
-            print()
+        if loss_val.item() < best_val:
+            # print("loss is ... ", loss_val.item())
+            best_val = loss_val.item()
+            print("best val loss is.. {}\t and saved at epoch {}\t ".format(best_val, epoch))
+            PATH = '../models/best_model_val_loss.pth'
+            torch.save(net.state_dict(), PATH)
 
     scheduler.step()
 
@@ -104,11 +83,11 @@ print('It took {} seconds to train the model.. '.format(time.time() - start_time
 # plot and save the losses
 fig = plt.figure(figsize=(10,5))
 plt.title("Training and Validation Loss")
-plt.plot(train_losses, label = "train")
-plt.plot(val_losses, label = "val")
+plt.plot(train_loss_history, label = "train")
+plt.plot(val_loss_history, label = "val")
 plt.xlabel("Epochs")
 plt.ylabel("Loss")
 plt.legend()
-fig.savefig('new_Train_&_Val_loss.png')
+fig.savefig('Train_Val_loss.png')
 
 
