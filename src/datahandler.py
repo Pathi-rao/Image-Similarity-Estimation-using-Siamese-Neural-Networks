32fa48c6bef169208b957b8d0ce13597eb750530
Lakshmipathi rao Devalla <lakshmipathirao.devalla@stud.uni-hannover.de>
2021-12-25 22:52:36 +0100

    Update datahandlers

--- src/datahandler.py
+++ src/datahandler.py
@@ -4,12 +4,48 @@ from torchvision import datasets, transforms
 
 import dataLoader as dl
 
-def pre_processor(root_dir, trainbatchsize, validbatchsize):
+"""
 
-    train_data = datasets.ImageFolder(root_dir + '/Train')
+A function which creates train and valid dataloaders that can be iterated over.
+To do so, you need to structure your data as follows:
+root_dir
+    |_train
+        |_class_1
+            |_xxx.png
+        .....
+        .....    
+        |_class_n
+            |_xxx.png
+    |_validation
+        |_class_1
+            |_xxx.png
+        .....
+        .....
+        |_class_n
+            |_xxx.png
+that means that each class has its own directory.
+By giving this structure, the name of the class will be taken by the name of the folder!
+
+Parameters
+----------
+root_dir: (str) "Path to where the data is"
+trainbatchsize: (int) batch size for training
+validbatchsize: (int) batch size for validation
+testbatchsize: (int) batch size for testing the model
+"""
+
+def pre_processor(root_dir, trainbatchsize, validbatchsize, testbatchsize):
+
+    train_data = datasets.ImageFolder(root_dir + '/New_train')
+    test_data = datasets.ImageFolder(root_dir + '/New_test')
 
     siamese_train_dataset = dl.SNNTrain(imageFolderDataset = train_data,
-                                            transform = transforms.Compose([transforms.Resize((28,28)),
+                                            transform = transforms.Compose([transforms.Resize((128,128)),
+                                                                            transforms.ToTensor()]),
+                                                                            # transforms.Normalize([0.4318, 0.4012, 0.3913], [0.2597, 0.2561, 0.2525])]),
+                                                                            should_invert = False)
+    siamese_test_dataset = dl.SNNTest(imageFolderDataset = test_data,
+                                            transform = transforms.Compose([transforms.Resize((128,128)),
                                                                             transforms.ToTensor()]),
                                                                             # transforms.Normalize([0.4318, 0.4012, 0.3913], [0.2597, 0.2561, 0.2525])]),
                                                                             should_invert = False)
@@ -20,13 +56,11 @@ def pre_processor(root_dir, trainbatchsize, validbatchsize):
 
     # create the dataloaders
     train_loader = DataLoader(train_set, batch_size = trainbatchsize,
-                                            shuffle = True)
+                                            shuffle = True
+                                            )
     valid_loader =  DataLoader(val_set, batch_size = validbatchsize,
-                                            shuffle = False) # shuffle doesn't matter during validation and testing
+                                            shuffle = False) # shuffle doesn't matter during validation and testing 
+    test_loader = DataLoader(siamese_test_dataset, batch_size = testbatchsize,
+                                            shuffle = True)
 
-    return train_loader , valid_loader
-
-
-# debug
-train_loader, valid_loader = pre_processor(root_dir='../../../data', trainbatchsize=4, 
-                                                validbatchsize=4)
+    return train_loader , valid_loader, test_loader
