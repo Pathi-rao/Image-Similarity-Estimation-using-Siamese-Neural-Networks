f5bcf1bff84263a077f5f04b8981b4b2234d37c5
Lakshmipathi rao Devalla <lakshmipathirao.devalla@stud.uni-hannover.de>
2022-01-16 22:40:35 +0100

    Update misc.py

--- src/misc.py
+++ src/misc.py
@@ -1,6 +1,7 @@
+from os import stat
 import matplotlib.pyplot as plt
 import numpy as np
-
+import torch
 
 class Utils:
     
@@ -20,3 +21,25 @@ class Utils:
     def show_plot(iteration, loss):
         plt.plot(iteration, loss)
         plt.show()
+
+    """saving and loading checkpoint mechanisms"""
+    @staticmethod
+    def save_checkpoint(save_path, model, optimizer, val_loss):
+        if save_path==None:
+            return
+        save_path = save_path 
+        state_dict = {'model_state_dict': model.state_dict(),
+                    'optimizer_state_dict': optimizer.state_dict(),
+                    'val_loss': val_loss}
+        torch.save(state_dict, save_path)
+        print(f'Model saved to ==> {save_path}')
+
+    def load_checkpoint(model, optimizer):
+        save_path = f'siameseNet-batchnorm50.pt'
+        state_dict = torch.load(save_path)
+        model.load_state_dict(state_dict['model_state_dict'])
+        optimizer.load_state_dict(state_dict['optimizer_state_dict'])
+        val_loss = state_dict['val_loss']
+        print(f'Model loaded from <== {save_path}')
+        
+        return val_loss
